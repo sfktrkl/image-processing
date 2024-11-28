67da557a396b828008db6e52251760c751b527da
Safak Turkeli <sfktrkl@gmail.com>
2024-11-29 01:04:35 +0300

    Move image reading and writing functions to a utility module

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,16 @@
 mod image_processing;
+mod utility;
 
-use image::{open, GrayImage, ImageBuffer, Luma};
 use image_processing::filters::{ImageFilter, SobelFilter};
 use image_processing::image_processor::ImageProcessor;
-use std::fs;
-
-fn list_input_output_image_files() -> Vec<(String, String)> {
-    let input_folder = "input";
-    let output_folder = "output";
-
-    let files = fs::read_dir(input_folder).expect("Failed to read input folder");
-
-    let mut paths = Vec::new();
-    for file in files {
-        let path = file.expect("Failed to read file").path();
-        if let Some(extension) = path.extension() {
-            if extension == "jpg" || extension == "png" || extension == "jpeg" {
-                if let Some(input_path) = path.to_str() {
-                    if let Some(file_name) = path.file_name() {
-                        let output_path =
-                            format!("{}/{}", output_folder, file_name.to_string_lossy());
-                        paths.push((input_path.to_string(), output_path));
-                    }
-                }
-            }
-        }
-    }
-
-    paths
-}
-
-fn read_image_file(file: &str) -> (Vec<f32>, (u32, u32)) {
-    let img = open(file).expect("Failed to open image").to_luma8();
-    (
-        img.pixels().map(|p| p[0] as f32).collect(),
-        img.dimensions(),
-    )
-}
-
-fn write_image_file(file: &str, pixels: Vec<f32>, width: u32, height: u32) {
-    // Normalize the output and save as an image
-    let max_val = pixels.iter().cloned().fold(0.0 / 0.0, f32::max); // Find max value
-    let output_image: GrayImage = ImageBuffer::from_fn(width, height, |x, y| {
-        let pixel_value = (pixels[(y * width + x) as usize] / max_val * 255.0) as u8;
-        Luma([pixel_value])
-    });
-
-    output_image
-        .save(file)
-        .expect("Failed to save output image");
-    println!("Sobel edge detection complete. Output saved to {}.", file);
-}
+use utility::Utility;
 
 fn main() {
-    let files = list_input_output_image_files();
+    let files = Utility::list_input_output_image_files();
     for (input, output) in &files {
-        let (input_pixels, (width, height)) = read_image_file(input);
+        let (input_pixels, (width, height)) = Utility::read_image_file(input);
         let sobel_processor = ImageProcessor::new(&input_pixels, width, height);
         let output_pixels = sobel_processor.process(SobelFilter::get_kernel());
-        write_image_file(&output, output_pixels, width, height);
+        Utility::write_image_file(&output, output_pixels, width, height);
     }
 }
---
+++ src/utility.rs
@@ -0,0 +1,53 @@
+use image::{open, GrayImage, Luma};
+use std::fs;
+
+pub struct Utility;
+
+impl Utility {
+    pub fn list_input_output_image_files() -> Vec<(String, String)> {
+        let input_folder = "input";
+        let output_folder = "output";
+
+        let files = fs::read_dir(input_folder).expect("Failed to read input folder");
+
+        let mut paths = Vec::new();
+        for file in files {
+            let path = file.expect("Failed to read file").path();
+            if let Some(extension) = path.extension() {
+                if extension == "jpg" || extension == "png" || extension == "jpeg" {
+                    if let Some(input_path) = path.to_str() {
+                        if let Some(file_name) = path.file_name() {
+                            let output_path =
+                                format!("{}/{}", output_folder, file_name.to_string_lossy());
+                            paths.push((input_path.to_string(), output_path));
+                        }
+                    }
+                }
+            }
+        }
+
+        paths
+    }
+
+    pub fn read_image_file(file: &str) -> (Vec<f32>, (u32, u32)) {
+        let img = open(file).expect("Failed to open image").to_luma8();
+        (
+            img.pixels().map(|p| p[0] as f32).collect(),
+            img.dimensions(),
+        )
+    }
+
+    pub fn write_image_file(file: &str, pixels: Vec<f32>, width: u32, height: u32) {
+        // Normalize the output and save as an image
+        let max_val = pixels.iter().cloned().fold(0.0 / 0.0, f32::max); // Find max value
+        let output_image: GrayImage = GrayImage::from_fn(width, height, |x, y| {
+            let pixel_value = (pixels[(y * width + x) as usize] / max_val * 255.0) as u8;
+            Luma([pixel_value])
+        });
+
+        output_image
+            .save(file)
+            .expect("Failed to save output image");
+        println!("Sobel edge detection complete. Output saved to {}.", file);
+    }
+}
