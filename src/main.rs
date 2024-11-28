dca8cf477aa462943d6ba2819ef712c4022c9ef5
Safak Turkeli <sfktrkl@gmail.com>
2024-11-29 01:39:58 +0300

    Spawn threads for each image will be processed

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ mod utility;
 
 use image_processing::filters::{ImageFilter, SobelFilter};
 use image_processing::image_processor::ImageProcessor;
+use std::thread;
 use utility::Utility;
 
 fn main() {
     let files = Utility::list_input_output_image_files();
-    for (input, output) in &files {
-        let (input_pixels, dimensions) = Utility::read_image_file(input);
-        let image_processor = ImageProcessor::new(&input_pixels, dimensions);
-        let sobel_filter_output = image_processor.process(SobelFilter::get_kernel());
-        Utility::write_image_file(&output, sobel_filter_output, dimensions);
+
+    let mut handles = vec![];
+    for (input, output) in files {
+        let handle = thread::spawn(move || {
+            let (input_pixels, dimensions) = Utility::read_image_file(&input);
+            let image_processor = ImageProcessor::new(&input_pixels, dimensions);
+            let sobel_filter_output = image_processor.process(SobelFilter::get_kernel());
+            Utility::write_image_file(&output, sobel_filter_output, dimensions);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().expect("Thread panicked");
     }
 }
