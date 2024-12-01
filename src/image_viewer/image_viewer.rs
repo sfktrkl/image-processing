184c9d23469e19aec72f694a4aee1a09b6893a45
Safak Turkeli <sfktrkl@gmail.com>
2024-12-01 05:41:49 +0300

    Initialize image viewer the buffer in new function

--- src/image_viewer/image_viewer.rs
+++ src/image_viewer/image_viewer.rs
@@ -16,8 +16,7 @@ pub struct Viewport {
 
 pub struct ImageViewer {
     window: Viewport,
-    cells: Vec<Cell>,
-    buffer: Option<Vec<u32>>,
+    buffer: Vec<u32>,
 }
 
 impl ImageViewer {
@@ -77,51 +76,47 @@ impl ImageViewer {
             window: popup,
         };
 
-        Self {
-            window,
-            cells,
-            buffer: None,
-        }
+        let buffer = Self::render(cells, window_width as usize, window_height as usize);
+
+        Self { window, buffer }
     }
 
     pub fn run(&mut self) {
-        if self.buffer.is_none() {
-            self.buffer = Some(self.render());
-        }
-
         let window = &mut self.window.window;
         while window.is_open() && !window.is_key_down(Key::Escape) {
-            let width = self.window.width as usize;
-            let height = self.window.height as usize;
-            window
-                .update_with_buffer(&self.buffer.as_ref().unwrap(), width, height)
-                .unwrap();
+            if let Err(error) = window.update_with_buffer(
+                &self.buffer,
+                self.window.width as usize,
+                self.window.height as usize,
+            ) {
+                eprintln!("Error updating buffer: {:?}", error);
+            }
         }
     }
 
-    fn render(&self) -> Vec<u32> {
-        let mut buffer = vec![0; (self.window.width * self.window.height) as usize];
+    fn render(cells: Vec<Cell>, width: usize, height: usize) -> Vec<u32> {
+        let mut buffer = vec![0; width * height];
 
-        for cell in &self.cells {
-            self.render_cell(&cell, &mut buffer);
+        for cell in cells {
+            Self::render_cell(&cell, &mut buffer, width, height);
         }
 
         buffer
     }
 
-    fn render_cell(&self, cell: &Cell, buffer: &mut [u32]) {
+    fn render_cell(cell: &Cell, buffer: &mut [u32], width: usize, height: usize) {
         let image_width = cell.width as usize;
         let image_height = cell.height as usize;
 
         for y in 0..image_height {
             let target_y = y + cell.y_offset as usize;
-            if target_y >= self.window.height as usize {
+            if target_y >= height as usize {
                 break;
             }
 
             for x in 0..image_width {
                 let target_x = x + cell.x_offset as usize;
-                if target_x >= self.window.width as usize {
+                if target_x >= width as usize {
                     break;
                 }
 
@@ -131,7 +126,7 @@ impl ImageViewer {
                 let g = (pixel >> 8 & 0xFF) as u8;
                 let b = (pixel & 0xFF) as u8;
 
-                let target_index = target_y * self.window.width as usize + target_x;
+                let target_index = target_y * width as usize + target_x;
                 buffer[target_index] = (r as u32) << 16 | (g as u32) << 8 | (b as u32);
             }
         }
