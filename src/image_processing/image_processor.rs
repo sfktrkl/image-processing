542699c5818d66f5bdef60c5224ee7ff7c9fe788
Safak TURKELI <sfktrkl@gmail.com>
2024-12-08 09:39:58 +0300

    Refactor image processor module

--- src/image_processing/image_processor.rs
+++ src/image_processing/image_processor.rs
@@ -22,15 +22,15 @@ impl<'a, 'b> ImageProcessor<'a, 'b> {
     }
 
     pub fn preprocess_image(
-        input: &[u32],
+        &self,
         filter: &Box<dyn ImageFilter>,
     ) -> ((Vec<f32>, Vec<f32>, Vec<f32>), Vec<f32>) {
-        let grayscale = ImageConverter::convert_rgb_to_grayscale(input);
+        let grayscale = ImageConverter::convert_rgb_to_grayscale(self.input);
         let options = filter.compute_options(&grayscale);
         let kernel = filter.get_kernel();
         match kernel.1 {
             "gaussianBlur" | "laplacianSharpening" | "bayerOrderedDithering" => {
-                let channels = ImageConverter::decompose_rgb(input);
+                let channels = ImageConverter::decompose_rgb(self.input);
                 (channels, options)
             }
             _ => ((grayscale, vec![], vec![]), options),
@@ -41,9 +41,8 @@ impl<'a, 'b> ImageProcessor<'a, 'b> {
         self.filters
             .iter()
             .map(|filter| {
-                let inputs = Self::preprocess_image(self.input, filter);
                 let kernel = filter.get_kernel();
-                let (channels, options) = inputs;
+                let (channels, options) = self.preprocess_image(filter);
 
                 let channels: Vec<Vec<f32>> = match kernel.1 {
                     "gaussianBlur" | "laplacianSharpening" | "bayerOrderedDithering" => {
@@ -62,19 +61,23 @@ impl<'a, 'b> ImageProcessor<'a, 'b> {
                     }
                 };
 
-                match kernel.1 {
-                    "gaussianBlur" | "bayerOrderedDithering" => {
-                        ImageConverter::recompose_rgb(&channels[0], &channels[1], &channels[2])
-                    }
-                    "laplacianSharpening" => ImageConverter::recompose_rgb_with_original(
-                        &channels[0],
-                        &channels[1],
-                        &channels[2],
-                        self.input,
-                    ),
-                    _ => ImageConverter::convert_grayscale_to_rgb(&channels[0]),
-                }
+                self.postprocess_image(&channels, kernel)
             })
             .collect()
+    }
+
+    pub fn postprocess_image(&self, channels: &[Vec<f32>], kernel: (&str, &str)) -> Vec<u32> {
+        match kernel.1 {
+            "gaussianBlur" | "bayerOrderedDithering" => {
+                ImageConverter::recompose_rgb(&channels[0], &channels[1], &channels[2])
+            }
+            "laplacianSharpening" => ImageConverter::recompose_rgb_with_original(
+                &channels[0],
+                &channels[1],
+                &channels[2],
+                self.input,
+            ),
+            _ => ImageConverter::convert_grayscale_to_rgb(&channels[0]),
+        }
     }
 }
