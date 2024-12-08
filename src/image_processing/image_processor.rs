65a2483ad2aefd0a9f4087b97e83628121a76cbc
Safak TURKELI <sfktrkl@gmail.com>
2024-12-08 09:37:52 +0300

    Move Preprocessor to image processor module

--- src/image_processing/image_processor.rs
+++ src/image_processing/image_processor.rs
@@ -1,7 +1,26 @@
 use super::filters::ImageFilter;
 use super::image_converter::ImageConverter;
 use super::opencl_processor::OpenCLProcessor;
-use super::preprocessor::Preprocessor;
+
+pub struct ImagePreProcessor;
+
+impl ImagePreProcessor {
+    pub fn prepare(
+        input: &[u32],
+        filter: &Box<dyn ImageFilter>,
+    ) -> ((Vec<f32>, Vec<f32>, Vec<f32>), Vec<f32>) {
+        let grayscale = ImageConverter::convert_rgb_to_grayscale(input);
+        let options = filter.compute_options(&grayscale);
+        let kernel = filter.get_kernel();
+        match kernel.1 {
+            "gaussianBlur" | "laplacianSharpening" | "bayerOrderedDithering" => {
+                let channels = ImageConverter::decompose_rgb(input);
+                (channels, options)
+            }
+            _ => ((grayscale, vec![], vec![]), options),
+        }
+    }
+}
 
 pub struct ImageProcessor<'a, 'b> {
     input: &'a [u32],
@@ -26,7 +45,7 @@ impl<'a, 'b> ImageProcessor<'a, 'b> {
         self.filters
             .iter()
             .map(|filter| {
-                let inputs = Preprocessor::prepare(self.input, filter);
+                let inputs = ImagePreProcessor::prepare(self.input, filter);
                 let kernel = filter.get_kernel();
                 let (channels, options) = inputs;
 
--- src/image_processing/mod.rs
+++ src/image_processing/mod.rs
@@ -2,4 +2,3 @@ pub mod filters;
 pub mod image_converter;
 pub mod image_processor;
 pub mod opencl_processor;
-pub mod preprocessor;
